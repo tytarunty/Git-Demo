c35841aa89fea63165d9660fbec79f0dca69bf34
Tarun <tytarunty@gmail.com>
2022-12-13 00:16:38 +0530

    Start froom begin and print link list

--- Data Structure/Link List/SinglyLinkList.c
+++ Data Structure/Link List/SinglyLinkList.c
@@ -1,34 +1,65 @@
 #include<stdio.h>
 
+#define CHECK_EMPTY_LIST(start) if(start == NULL) \
+                                    { printf("%s","Empty Link List\n"); \
+                                        return ; }
+
 struct node {
     int data;
-    struct node * ; 
+    struct node * link; 
 };
+
+struct node* InsertBegining(struct node *start, int data);
+void PrintLinkList(struct node *start);
+
 
 void main()
 {
     struct node *start=NULL;
-    int choice;
+    int choice,data;
 
     while (1)
     {
         printf("1. Insert from begining\n");
-        
+        printf("2. Print Link List\n");
+        printf("10. Exit\n");
         scanf("%d",&choice);
         switch (choice)
         {
         case 1:
-            InsertBegining(start);
+            printf("Enter Data : ");
+            scanf("%d",&data);
+            start = InsertBegining(start,data);
             break;
+        case 2:
+            PrintLinkList(start);
+            printf("\n");
+            break;
+        case 10:
+            exit(0);
         default:
             printf("Enter correct choice\n");
         }
-        printf("1. ")
     }
     return 0;
 }
 
-void InsertBegining(struct node *start)
+struct node* InsertBegining(struct node *start, int data)
 {
+    struct node *temp;
+    temp = (struct node *) malloc(sizeof(struct node));
+    temp->data = data;
+    temp->link = start;
+    return temp;
+}
+
+void PrintLinkList(struct node *start)
+{
+    CHECK_EMPTY_LIST(start)
     
+    while (start != NULL)
+    {
+        printf("%d --> ",start->data);
+        start=start->link;
+    }
 }
