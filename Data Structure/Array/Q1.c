7f467eaf76f0b35ca3c87ae83fc9c7206c1ba8d9
“tarun” <tytarunty@gmail.com>
2023-10-09 20:18:40 +0530

    Largest three elements

---
+++ Data Structure/Array/Q1.c
@@ -0,0 +1,35 @@
+// WAP to Find the largest three distinct elements in an array
+// Input: arr[] = {10, 4, 3, 50, 23, 90}
+
+#include<stdio.h>
+#include <limits.h> /* For INT_MIN */
+
+void Print3Largest(int arr[],int n)
+{
+    int first,second,third;
+    first=second=third=INT_MIN;
+    for (int i = 0 ; i < n ; i++){
+        if (arr[i] > first){
+            third  = second;
+            second = first;
+            first  = arr[i];
+        }
+        else if ( arr[i] > second){
+            third  = second;
+            second = arr[i];
+        }
+        else if ( arr > third ){
+            third = arr[i];
+        }
+    }
+
+    printf("first : %d , second : %d , third : %d\n",first,second,third);
+}
+
+int main()
+{
+    int arr[]={10, 4, 3, 50, 23, 90};
+    int n = sizeof(arr)/sizeof(int);
+    Print3Largest(arr,n);
+    return 0;
+}
