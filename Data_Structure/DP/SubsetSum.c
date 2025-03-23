490e858f9b607a797040a09ed40e64771278426b
“tarun” <tytarunty@gmail.com>
2025-03-23 17:13:45 +0530

    subset sum DP
    Signed-off-by: “tarun” <“tytarunty@gmail.com”>

--- Data_Structure/DP/SubsetSum.c
+++ Data_Structure/DP/SubsetSum.c
@@ -55,8 +55,8 @@ int knapsack_topdown(int wt[],int W,int n){
     
     for(int i = 1 ; i < n+1 ; i++){
         for (int j = 1; j < W+1; j++){
-            if(wt[i-1] <= j)
-            dp[i][j] =  dp[i-1][j-wt[i-1]] + dp[i-1][j];
+        if(wt[i-1] <= j)
+            dp[i][j] =  dp[i-1][j-wt[i-1]] || dp[i-1][j];
         else if (wt[n-1] > j)
             dp[i][j] = dp[i-1][j];
         }        
